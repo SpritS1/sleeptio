5686e503e8354bf1703f731be964449e7eb57c59
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2021-09-10 11:45:07 +0200

    Update Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
 const Navbar = () => {
     return ( 
         <nav className='navbar'>
-            <i className="fas fa-bed"></i>
+            {/* <i className="fas fa-bed"></i> */}
+            <a href='/'className="navbar__title">Sleeptio</a>
         </nav>
      );
 }
