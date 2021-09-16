5d9987e0e0967ff88e87dc028fbe305c37207f27
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2021-09-16 21:48:58 +0200

    Update caluclation results styling

--- src/components/Calculator/Hour.js
+++ src/components/Calculator/Hour.js
@@ -1,6 +1,9 @@
 const Hour = ({hour}) => {
     return ( 
-        <li className='hour'>{hour}</li>
+        <li className='hour'>
+            <h4 className="hour__time">{hour}</h4>
+            <p className="hour__info">9 hours of sleep, 6 sleep cycles</p>
+        </li>
      );
 }
  
