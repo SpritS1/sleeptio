3094ddb243b196e9127d030f067849b78acb4cd8
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2021-09-17 12:39:28 +0200

    Changed result display in CurrentTimeResults to HoursList

--- src/components/CurrentTimeResults.js
+++ src/components/CurrentTimeResults.js
@@ -1,24 +1,32 @@
 import moment from "moment";
 import { useEffect, useState } from "react";
+import HoursList from './Calculator/HoursList';
 
 const CurrentTimeResults = (props) => {
     const calcWakeTime = (time) => {
-        const result = moment(time).add(90 * 6, 'minutes')._d;
-        return moment(result).format('HH:mm');   
+        const sleepTime = [];
+        for (let i = 5; i <= 6; i++) {
+            const result = moment(time).add(90 * i, 'minutes')._d;
+            sleepTime.push(moment(result).format('HH:mm'))
+        }
+        console.log(sleepTime)
+        return sleepTime.reverse();   
     };
 
     const [currentTime, setCurrentTime] = useState(moment(new Date()))
-    const [wakeTime, setWakeTime] = useState('');
+    const [resultHours, setResultHours] = useState('');
     
     useEffect(() => {
-        setWakeTime(() => calcWakeTime(currentTime))
+        setResultHours(() => calcWakeTime(currentTime))
         setTimeout(() => setCurrentTime(new Date()), 60000)
     }, [currentTime])
 
     return ( 
         <div className="current-time-results">
-            <h2>If you go to sleep now you should wake up at:</h2>
-            <span className='current-time-results__result'>{wakeTime}</span>
+            <h2>If you go to sleep in 15 minutes you should wake up at:</h2>
+            <HoursList resultHours={resultHours}></HoursList>
+            {/* <span className='current-time-results__result'>{wakeTime}</span>
+            <span className='current-time-results__result'>{wakeTime}</span> */}
         </div>
      );
 }
